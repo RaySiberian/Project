6d557c14babc58463d3628f2c2c3138fb1399f60
RaySiberian <81709760+RaySiberian@users.noreply.github.com>
2021-08-23 06:21:07 +0300

    Revert "Revert "Project""
    This reverts commit acf500daedcb19d9ee16a1d1068d55729b3616ba.

---
+++ Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public ItemObject item;
+
+    
+    //TODO Заменить на нормальное условие
+    // private void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.F1))
+    //     {
+    //         Player player = GameObject.FindObjectOfType<Player>();
+    //         player.Inventory.AddItem(item,1);
+    //         Destroy(gameObject);
+    //     }
+    // }
+}
