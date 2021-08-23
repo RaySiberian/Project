ada2f92bdc2e287a3c2b0dde56ee0e6fec2f00f7
RaySiberian <81709760+RaySiberian@users.noreply.github.com>
2021-08-23 06:19:45 +0300

    Revert "Started Inventory Implementation"
    This reverts commit d8c9d392eafc3dbbf8f293360482062be877d1f1.

--- Assets/Scripts/Inventory/InventoryObject.cs
+++ Assets/Scripts/Inventory/InventoryObject.cs
@@ -1,68 +1,28 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using UnityEditor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Inventory Object", menuName = "Inventory System/Inventory")]
-public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
+public class InventoryObject : ScriptableObject
 {
-    public string savePath;
-    private ItemDatabaseObject Database;
     public List<InventorySlot> Container = new List<InventorySlot>();
-
-    private void OnEnable()
-    {
-        //TODO из-за этого игра не забилдится 
-        Database = (ItemDatabaseObject)AssetDatabase.LoadAssetAtPath("InventoryAssest/Database.asset",
-            typeof(ItemDatabaseObject));
-    }
 
     public void AddItem(ItemObject addingItem, int addingAmount)
     {
+        bool hasItem = false;
+        
         for (int i = 0; i < Container.Count; i++)
         {
             if (Container[i].item == addingItem)
             {
                 Container[i].AddAmount(addingAmount);
-                return;
+                hasItem = true;
+                break;
             }
         }
 
-        //TODO разобраться с Database.GetId[addingItem]
-        Container.Add(new InventorySlot(Database.GetIdByItem[addingItem], addingItem, addingAmount));
-    }
-
-    public void Save()
-    {
-        string saveData = JsonUtility.ToJson(this, true);
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(string.Concat(Application.persistentDataPath, savePath));
-        bf.Serialize(file, saveData);
-        file.Close();
-    }
-
-    public void Load()
-    {
-        if (File.Exists(string.Concat(Application.persistentDataPath, savePath)))
+        if (!hasItem)
         {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(string.Concat(Application.persistentDataPath, savePath), FileMode.Open);
-            JsonUtility.FromJsonOverwrite(bf.Deserialize(file).ToString(), this);
-            file.Close();
+            Container.Add(new InventorySlot(addingItem,addingAmount));
         }
-    }
-
-    public void OnAfterDeserialize()
-    {
-        for (int i = 0; i < Container.Count; i++)
-        {
-            Container[i].item = Database.GetItemById[Container[i].ID];
-        }
-    }
-
-    public void OnBeforeSerialize()
-    {
     }
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,21 +1,6 @@
-using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
    public InventoryObject Inventory;
-
-   private void Update()
-   {
-      if (Input.GetKeyDown(KeyCode.K))
-      {
-         Inventory.Save();
-      }
-      
-      if (Input.GetKeyDown(KeyCode.L))
-      {
-         Inventory.Load();
-      }
-
-   }
 }
