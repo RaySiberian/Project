0338a2dd3339518562fe40051e5d5834abfee02a
RaySiberian <81709760+RaySiberian@users.noreply.github.com>
2021-09-03 17:03:24 +0300

    Old Inventory System Deleted

--- Assets/Scripts/Inventory/ScriptableObj/EquipmentObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory System/Items/Equipment")]
-public class EquipmentObject : ItemObject
-{
-   //TODO расширить класс на отдельные элементы экиперовки
-}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public InventoryObject Inventory;
-    public InventoryObject Equipment;
-    public Attribute[] Attributes;
-
     private GameObject rightHandItem ;
     private GameObject leftHandItem ;
     private float rayCastRange = 5f;
@@ -19,34 +15,8 @@ public class Player : MonoBehaviour
         mainCamera = Camera.main;
     }
 
-    private void Start()
-    {
-        for (int i = 0; i < Attributes.Length; i++)
-        {
-            Attributes[i].SetParent(this);
-        }
-
-        for (int i = 0; i < Equipment.GetSlots.Length; i++)
-        {
-            Equipment.GetSlots[i].OnBeforeUpdate += OnRemoveItem;
-            Equipment.GetSlots[i].OnAfterUpdate += OnAddItem;
-        }
-    }
-
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-        {
-            Inventory.Load();
-            Equipment.Load();
-        }
-
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Inventory.Save();
-            Equipment.Save();
-        }
-
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shot();
@@ -64,106 +34,17 @@ public class Player : MonoBehaviour
             }
         }
     }
-    
-    public void OnRemoveItem(InventorySlot slot)
-    {
-        if (slot.ItemObject == null)
-        {
-            return;
-        }
-        
-        switch (slot.Parent.inventory.Type)
-        {
-            case InterfaceType.Inventory:
-                break;
-            case InterfaceType.Equipment:
-                for (int i = 0; i < slot.Item.buffs.Length; i++)
-                {
-                    for (int j = 0; j < Attributes.Length; j++)
-                    {
-                        if (Attributes[j].type == slot.Item.buffs[i].attribute)
-                        {
-                            Attributes[j].value.RemoveModifier(slot.Item.buffs[i]);
-                        }
-                    }
-                }
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     var item = other.GetComponent<GroundItem>();
+    //     if (item)
+    //     {
+    //         Item _item = new Item(item.item);
+    //         if (Inventory.AddItem(_item, 1))
+    //         {
+    //             Destroy(other.gameObject);
+    //         }
+    //     }
+    // }
 
-                if (slot.ItemObject.type == ItemType.Weapon && rightHandItem != null)
-                {
-                    Destroy(rightHandItem);
-                }
-                
-                if (slot.ItemObject.type == ItemType.Shield && leftHandItem != null)
-                {
-                    Destroy(leftHandItem);
-                }
-                
-                break;
-            case InterfaceType.Chest:
-                break;
-        }
-    }
-
-    public void OnAddItem(InventorySlot slot)
-    {
-        if (slot.ItemObject == null)
-        {
-            return;
-        }
-        
-        switch (slot.Parent.inventory.Type)
-        {
-            case InterfaceType.Inventory:
-                break;
-            case InterfaceType.Equipment:
-                for (int i = 0; i < slot.Item.buffs.Length; i++)
-                {
-                    for (int j = 0; j < Attributes.Length; j++)
-                    {
-                        if (Attributes[j].type == slot.Item.buffs[i].attribute)
-                        {
-                            Attributes[j].value.AddModifier(slot.Item.buffs[i]);
-                        }
-                    }
-                }
-
-                if (slot.ItemObject.characterDisplay != null)
-                {
-                    //Debug.Log(slot.SlotDisplay.name);
-                    if (slot.ItemObject.type == ItemType.Shield)
-                    { 
-                        leftHandItem = Instantiate(slot.ItemObject.characterDisplay, leftHandShieldTransform);
-                    }
-
-                    if (slot.ItemObject.type == ItemType.Weapon)
-                    {
-                        rightHandItem = Instantiate(slot.ItemObject.characterDisplay, rightHandTransform);
-                    }
-                }
-                
-                break;
-            case InterfaceType.Chest:
-                break;
-        }
-    }
-    
-    
-    private void OnTriggerEnter(Collider other)
-    {
-        var item = other.GetComponent<GroundItem>();
-        if (item)
-        {
-            Item _item = new Item(item.item);
-            if (Inventory.AddItem(_item, 1))
-            {
-                Destroy(other.gameObject);
-            }
-        }
-    }
-
-    public void AttributeModified(Attribute attribute)
-    {
-        //Debug.Log(string.Concat(attribute.type, "was updated! Value is now", attribute.value.ModifiedValue));
-    }
-    
 }
